4b154de9f6b9fa01e6bd11093d7a3a5836da22e3
Bruno César <>
2021-03-02 11:58:03 -0300

    Buscando endereço pelo cep OK

--- assets/sitePrincipal/js/sitePrincipal/getCep.js
+++ assets/sitePrincipal/js/sitePrincipal/getCep.js
@@ -1,17 +1,24 @@
-$("#cep_usu").blur(function(){
-    var cep = $("#cep_usu").val().split("-");
-    cep = cep[0]+cep[1];
+$('#cep_usu').on('keyup', function(){
 
-    $.ajax({
-        url: '/consulta-cep',
-        type: 'POST',
-        data: {
-            cep:cep
-        },
-        dataType: 'json',
-        success: function(json){
-            
-        }
-    });
+    if($('#cep_usu').val().length == 9){
+        var cep = $("#cep_usu").val().split("-");
+        cep = cep[0]+cep[1];
+    
+        $.ajax({
+            url: '/consulta-cep',
+            type: 'POST',
+            data: {
+                cep:cep
+            },
+            dataType: 'json',
+            success: function(json){
+                $('#bairro_usu').val(json.bairro);
+                $('#rua_usu').val(json.logradouro);
+                $('#estado_usu option:selected').text(json.uf);
+                $('#cidade_usu').val(json.localidade);
+                $('#complemento').val(json.complemento);
+            }
+        });
+    }
 
 });
