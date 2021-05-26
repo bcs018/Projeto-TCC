21170dff5e895ca6a98e10cc85ff3c9f59f5e003
Bruno César <57082742+bcs018@users.noreply.github.com>
2021-05-26 15:31:09 -0300

    Adicionando template lay01

---
+++ src/models/commerce/Home.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace src\models\commerce;
+
+use \core\Model;
+
+class Assinatura extends Model{
+
+    // Pega todas as informações do site do ecommerce definido na url
+    public function pegaDadosCommerce(){
+
+    }
+
+}
