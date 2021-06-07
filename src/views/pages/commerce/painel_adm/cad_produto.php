2487720101af289a65973694cf5f517f3de003ce
Bruno César <57082742+bcs018@users.noreply.github.com>
2021-06-07 12:02:47 -0300

    Criando pagina de cadastro 2.0

--- src/views/pages/commerce/painel_adm/cad_produto.php
+++ src/views/pages/commerce/painel_adm/cad_produto.php
@@ -35,21 +35,90 @@ $render("commerce/header_painel", ['title'=>'Painel administrativo | Cadastrar P
                         <form role="form">
                             <div class="card-body">
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Nome produto</label>
-                                    <input type="text" class="form-control" id="nomeProd" name="nomeProd" placeholder="Insira o nome do produto">
+                                    <label for="nomeProd">Nome produto</label>
+                                    <input type="text" class="form-control" id="nomeProd" name="nomeProd"
+                                        placeholder="Insira o nome do produto">
                                 </div>
                                 <div class="form-group">
                                     <label for="descProd">Descrição do produto</label>
                                     <textarea name="descProd" id="descProd" rows="6" class="form-control"></textarea>
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Estoque</label>
-                                    <input type="number" class="form-control" id="estoque" name="estoque" placeholder="Insira o nome do produto">
+                                    <label for="estoque">Estoque</label>
+                                    <input type="number" class="form-control" id="estoque" name="estoque"
+                                        placeholder="Insira o nome do produto">
                                 </div>
                                 <div class="form-group">
-                                    <label for="exampleInputEmail1">Preço</label>
-                                    <input type="text" class="form-control" id="preco" name="preco" placeholder="Insira o preço do produto">
+                                    <label for="preco">Preço</label>
+                                    <div class="input-group">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text" id="">R$</span>
+                                        </div>
+                                        <input type="text" class="form-control" id="preco" name="preco"
+                                            placeholder="Insira o preço do produto">
+                                    </div>
                                 </div>
+                                <div class="form-group">
+                                    <label for="precoAnt">Preço antigo</label>
+                                    <div class="input-group">
+                                        <div class="input-group-append">
+                                            <span class="input-group-text" id="">R$</span>
+                                        </div>
+                                        <input type="text" class="form-control" id="precoAnt" name="precoAnt"
+                                            placeholder="Insira o preço antigo do produto">
+                                    </div>
+                                </div>
+
+                                <div class="card card-lightblue">
+                                    <div class="card-header">
+                                        <h3 class="card-title"><b>O que é Preço antigo?</b></h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <h6>
+                                            O campo Preço antigo é de uso opcional e serve para dar evidência ao
+                                            produto, por exemplo:
+                                        </h6>
+                                        <h6>
+                                            Produdo XYZ de <b>R$150,00</b> por R$119,99.
+                                        </h6>
+                                        <h6>
+                                            O preço de <b>R$150,00</b> é o preço antigo do produto.
+                                        </h6>
+                                        <h6>
+                                            O site somente irá informar o preço antigo se o produdo tiver o preço antigo
+                                            cadastrado.
+                                        </h6>
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="promo">Produto em promoção?</label>
+
+                                    <div class="custom-control custom-radio">
+                                        <input class="custom-control-input" type="radio" id="promoSim"
+                                            name="promo">
+                                        <label for="promoSim" class="custom-control-label">Sim</label>
+                                    </div>
+                                    <div class="custom-control custom-radio">
+                                        <input class="custom-control-input" type="radio" id="promoNao"
+                                            name="promo" checked="">
+                                        <label for="promoNao" class="custom-control-label">Não</label>
+                                    </div>
+                                </div>
+
+                                <div class="card card-lightblue">
+                                    <div class="card-header">
+                                        <h3 class="card-title"><b>O que é Produto em promoção?</b></h3>
+                                    </div>
+                                    <div class="card-body">
+                                        <h6>
+                                            Quando marcado <b>SIM</b>, o produto terá um destaque na loja e será um dos primeiros a aparecer na página principal, e terá um emblemano anuncio como "Promoção".
+                                        </h6>
+                                    </div>
+                                </div>
+
+
+
                                 <div class="form-group">
                                     <label for="exampleInputFile">File input</label>
                                     <div class="input-group">
@@ -84,7 +153,9 @@ $render("commerce/header_painel", ['title'=>'Painel administrativo | Cadastrar P
 </div>
 
 <script type="text/javascript">
-    $('#preco').mask("#.##0,00", {reverse: true});
-</script>  
+    $('#preco').mask("#.##0,00", {
+        reverse: true
+    });
+</script>
 
 <?php $render("commerce/footer_painel"); ?>
