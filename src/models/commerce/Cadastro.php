2c113618c6f7ac43e0e7fe49e9cf676a3f2ca9f0
Bruno César <57082742+bcs018@users.noreply.github.com>
2021-10-14 12:35:38 -0300

    Correções de erros

--- src/models/commerce/Cadastro.php
+++ src/models/commerce/Cadastro.php
@@ -151,7 +151,7 @@ class Cadastro extends Model{
                 $_SESSION['message'] .= '<div class="alert alert-success" role="alert">
                                             Usuário cadastrado com sucesso!
                                         </div>';
-                $_SESSION['login_cliente_ecommerce'] = $idUsu['ult'];
+                //$_SESSION['login_cliente_ecommerce'] = $idUsu['ult'];
 
                return true;
             }
